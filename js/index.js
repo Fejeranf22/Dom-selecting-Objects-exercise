7a1a5c0f6fb7fae483d35ec73ce6c595e17c99d7
ty fejerang <fejerang.ty@gmail.com>
2018-01-27 14:26:52 -0800

    in this initial commit i am building the the html and laying out the objectives all commented on the index.js page

---
+++ js/index.js
@@ -0,0 +1,26 @@
+// given the html select the first paragraph int the list 4 different ways and populate to the console.
+// alert("hello this is connected");
+
+//  #1 select element by the getElementById method
+var slct = document.getElementById("initial");
+// log to console
+console.log(" selection #1");
+console.log(slct);
+
+// #2 select element by the getElementByClass method
+var slctr = document.getElementsByClassName("especial")[0];
+// log to console
+console.log(" selection #2");
+console.log(slctr);
+
+//#3 select element by the getElementById method
+var slcto = document.querySelector("#initial");
+// log to console
+console.log(" selection #3");
+console.log(slcto);
+
+//#4 select element by the getElementById method
+var slcta = document.getElementsByTagName("li")[0];
+// log to console
+console.log(" selection #4");
+console.log(slcta);
